475ba13c3b7a0a84deb19cfbaba6d683d3dfb209
韓維 <hanwei@hans-mac.lan>
2022-10-16 02:55:43 -0500

    deploy

--- src/data.js
+++ src/data.js
@@ -1,35 +1,41 @@
 export const projects = [
   {
-    title: "React Reserve",
-    subtitle: "MERN Stack",
+    title: "Vehicles’ Intention Detection",
+    subtitle: "Computer Vision Research Assistant in Digital and Robotic Construction (DARC) Lab",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "Developed algorithm for vehicle turn signal recognition at intersections and roadsides that achieved 80% accuracy.",
     image: "./project-1.gif",
-    link: "https://reactbootcamp.com",
+    link: "https://www.youtube.com/watch?v=o1kk6lh-UYk",
   },
   {
-    title: "React Tracks",
-    subtitle: "React and Python",
+    title: "Robot Arms Control",
+    subtitle: "Course Project: Controlled robot arm to draw what a webcam sees in the view by ROS",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "- Utilized webcam to capture hand drawing and processed it as way points as a SVG file.\n"
+      +"- Calculated the angles of every robot arm joints by inverse kinematic to reach each way point.",
     image: "./project-2.gif",
-    link: "https://reactbootcamp.com",
+    link: "https://www.youtube.com/watch?v=9dyHZUbxACI",
   },
   {
-    title: "DevChat",
-    subtitle: "React and Firebase",
+    title: "2D and 3D Rebar Semantic Segmentation",
+    subtitle: "Computer Vision Research Assistant: Image and Point Cloud Segmentation",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "- Referenced Unet and transformer-in-transformer module to develop a semantic segmentation model, TNTUnet.\n"
+      +"- TNTUnet achieved 0.68 mIoU for detecting rebar spacings and intersections on rebar image dataset.\n"
+      +"- Modified Point Transformer by adding domain adaptation concept to create new deep learning model, DAPT.\n"
+      +"- DAPT was trained by labeled virtual data and unlabel real data to detect real rebar point cloud, achieving 74% accuracy.",
     image: "./project-3.gif",
-    link: "https://reactbootcamp.com",
+    link: "https://drive.google.com/file/d/12zitbdBUkW74rh7G0sHaH4bJQtSB92Rk/view",
   },
   {
-    title: "Epic Todo App",
-    subtitle: "React Hooks",
+    title: "Applying Image Analytics in Construction Site for Safety Monitoring",
+    subtitle: "Computer Vision Research Assistant: Object Detection and Multi-Object Tracking(MOT)",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "Detected and tracked workers on construction sites by Faster R-CNN (object detection) and DeepSORT (MOT).\n"
+      +"Obtained polygon by detected traffic cones to define hazard zone.\n"
+      +"Alerted when the workers go across the hazard zones.",
     image: "./project-4.gif",
-    link: "https://reactbootcamp.com",
+    link: "https://drive.google.com/file/d/1gwTpcHV1hJJxjk1OHTUAa8eVVjESaGXb/view",
   },
 ];
 
