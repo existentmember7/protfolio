dbd18c603431ec38b4b314dc797075411cf0d9c1
韓維 <hanwei@hans-mac.lan>
2022-10-16 03:57:08 -0500

    deploy

--- src/components/About.js
+++ src/components/About.js
@@ -24,10 +24,9 @@ export default function About() {
             </a>
           </div>
         </div>
-        <div className="lh-24 md:w-24 md:w-1/2 w-5/6">
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
           <img
-
-            className="object-cover object-center rounded"
+            className="lh-24 md:w-24 object-cover object-center rounded"
             alt="hero"
             src="./profile.jpg"
           />
