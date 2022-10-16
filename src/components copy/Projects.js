48c3e7e1e39e9ccb06cefa71e6ed472de2e2bfe1
韓維 <hanwei@hans-mac.lan>
2022-10-16 04:39:26 -0500

    deploy

--- src/components copy/Projects.js
+++ src/components copy/Projects.js
@@ -11,28 +11,31 @@ export default function Projects() {
           <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
             Professional Expirence
           </h1>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+            ..
+          </p>
         </div>
         <div className="flex flex-wrap -m-4">
           {projects.map((project) => (
-              <a
+            <a
               href={project.link}
               key={project.image}
               className="sm:w-1/2 w-100 p-4">
               <div className="flex relative">
                 <img
                   alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center"
+                  className="absolute inset-0 w-60 h-48 md:w-1/2 w-1/6 object-cover object-center"
                   src={project.image}
                 />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
-                  <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
-                    {project.subtitle}
-                  </h2>
-                  <h1 className="title-font text-lg font-medium text-white mb-3">
-                    {project.title}
-                  </h1>
-                  <p className="leading-relaxed">{project.description}</p>
-                </div>
+              </div>
+              <div className="px-8 py-10 relative z-10 w-60 h-48 md:w-1/2 w-1/2 border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
+                  {project.subtitle}
+                </h2>
+                <h1 className="title-font text-lg font-medium text-white mb-3">
+                  {project.title}
+                </h1>
+                <p className="leading-relaxed">{project.description}</p>
               </div>
             </a>
           ))}
