08557d628ff034c3972e7dd12d1fb4dbc520d5b6
韓維 <hanwei@hans-mac.lan>
2022-10-16 23:16:57 -0500

    deploy

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -14,27 +14,25 @@ export default function Projects() {
         </div>
         <div className="flex flex-wrap -m-4">
           {projects.map((project) => (
-              <a
-              href={project.link}
-              key={project.image}
-              className="sm:w-1/2 w-100 p-4">
-              <div className="flex relative">
+            <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+              <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                 <img
-                  alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center"
+                  className="w-full h-full"
                   src={project.image}
                 />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
-                  <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
-                    {project.subtitle}
-                  </h2>
-                  <h1 className="title-font text-lg font-medium text-white mb-3">
-                    {project.title}
-                  </h1>
-                  <p className="leading-relaxed">{project.description}</p>
-                </div>
               </div>
-            </a>
+              <div className="lg:w-full lg:w-full md:w-full w-full">
+                <h1 className="title-font text-3xl font-lg text-white mb-3">
+                  {project.title}
+                </h1>
+                <h1 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
+                  {project.subtitle}
+                </h1>
+                {project.description.map(description => (
+                  <p className="leading-relaxed text-left">{description}</p>
+                ))}
+              </div>
+            </div>
           ))}
         </div>
       </div>
--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export const projects = [
     title: "Vehicles’ Intention Detection",
     subtitle: "Computer Vision Research Assistant in Digital and Robotic Construction (DARC) Lab",
     description:
-      "Developed algorithm for vehicle turn signal recognition at intersections and roadsides that achieved 80% accuracy.",
+      ["Developed algorithm for vehicle turn signal recognition at intersections and roadsides that achieved 80% accuracy."],
     image: "./project-1.gif",
     link: "https://www.youtube.com/watch?v=o1kk6lh-UYk",
   },
@@ -11,8 +11,8 @@ export const projects = [
     title: "Robot Arms Control",
     subtitle: "Course Project: Controlled robot arm to draw what a webcam sees in the view by ROS",
     description:
-      "- Utilized webcam to capture hand drawing and processed it as way points as a SVG file.\n"
-      +"- Calculated the angles of every robot arm joints by inverse kinematic to reach each way point.",
+      ["- Utilized webcam to capture hand drawing and processed it as way points as a SVG file."
+      ,"- Calculated the angles of every robot arm joints by inverse kinematic to reach each way point."],
     image: "./project-2.gif",
     link: "https://www.youtube.com/watch?v=9dyHZUbxACI",
   },
@@ -20,10 +20,10 @@ export const projects = [
     title: "2D and 3D Rebar Semantic Segmentation",
     subtitle: "Computer Vision Research Assistant: Image and Point Cloud Segmentation",
     description:
-      "- Referenced Unet and transformer-in-transformer module to develop a semantic segmentation model, TNTUnet.\n"
-      +"- TNTUnet achieved 0.68 mIoU for detecting rebar spacings and intersections on rebar image dataset.\n"
-      +"- Modified Point Transformer by adding domain adaptation concept to create new deep learning model, DAPT.\n"
-      +"- DAPT was trained by labeled virtual data and unlabel real data to detect real rebar point cloud, achieving 74% accuracy.",
+      ["- Referenced Unet and transformer-in-transformer module to develop a semantic segmentation model, TNTUnet."
+      ,"- TNTUnet achieved 0.68 mIoU for detecting rebar spacings and intersections on rebar image dataset."
+      ,"- Modified Point Transformer by adding domain adaptation concept to create new deep learning model, DAPT."
+      ,"- DAPT was trained by labeled virtual data and unlabel real data to detect real rebar point cloud, achieving 74% accuracy."],
     image: "./project-3.gif",
     link: "https://drive.google.com/file/d/12zitbdBUkW74rh7G0sHaH4bJQtSB92Rk/view",
   },
@@ -31,9 +31,9 @@ export const projects = [
     title: "Applying Image Analytics in Construction Site for Safety Monitoring",
     subtitle: "Computer Vision Research Assistant: Object Detection and Multi-Object Tracking(MOT)",
     description:
-      "- Detected and tracked workers on construction sites by Faster R-CNN (object detection) and DeepSORT (MOT).\n"
-      +"- Obtained polygon by detected traffic cones to define hazard zone.\n"
-      +"- Alerted when the workers go across the hazard zones.",
+      ["- Detected and tracked workers on construction sites by Faster R-CNN (object detection) and DeepSORT (MOT)."
+      ,"- Obtained polygon by detected traffic cones to define hazard zone."
+      ,"- Alerted when the workers go across the hazard zones."],
     image: "./project-4.gif",
     link: "https://drive.google.com/file/d/1gwTpcHV1hJJxjk1OHTUAa8eVVjESaGXb/view",
   },
@@ -41,7 +41,7 @@ export const projects = [
     title: "A Semantic Segmentation Model for Autonomous Driving",
     subtitle: "Course Project: Applied TNTUnet on street-view dataset",
     description:
-      "TNTUnet achieve 0.61 mIoU on public dataset KITTI.",
+      ["TNTUnet achieve 0.61 mIoU on public dataset KITTI."],
     image: "./project-4.gif",
     link: "https://www.youtube.com/watch?v=ZOYQyxzb7CM",
   },
@@ -49,8 +49,8 @@ export const projects = [
     title: "Taiwan Semiconductor Manufacturing Company (TSMC) - Intern Program",
     subtitle: "Internship (Full-time): Optimizing chillers control for energy efficiency",
     description:
-      "- Collected environmental and electricity data from semiconductor fabrication plants"
-      +"- Utilized fully connection neural networks and grid search to predicted the best setting temperatures of chillers.",
+      ["- Collected environmental and electricity data from semiconductor fabrication plants"
+      ,"- Utilized fully connection neural networks and grid search to predicted the best setting temperatures of chillers."],
     image: "./project-4.gif",
     link: "",
   },
@@ -58,7 +58,7 @@ export const projects = [
     title: "Train Information and Management System",
     subtitle: "Software Engineer: Broadcast and Management of Voice Information",
     description:
-      "Developed train information announcement broadcasting and management system.",
+      ["Developed train information announcement broadcasting and management system."],
     image: "./project-4.gif",
     link: "https://drive.google.com/file/d/1gwSo-0AD6bBx8WN9tycVK4WXnXnLQIsB/view?usp=sharing",
   },
@@ -66,8 +66,8 @@ export const projects = [
     title: "Web Retrieval and Mining Final Project Report – Restaurant Recommendation System",
     subtitle: "Group Member: Web crawling and recommandation system.",
     description:
-      "- Crawling the restaurant information from the Tripadvisor website."
-      +"- Constructing Vector Space Model and using jieba(term processing) to recommend suitable restaurants for the users.",
+      ["- Crawling the restaurant information from the Tripadvisor website."
+      ,"- Constructing Vector Space Model and using jieba(term processing) to recommend suitable restaurants for the users."],
     image: "./project-4.gif",
     link: "https://github.com/existentmember7/restaurant_recommend_system",
   },
