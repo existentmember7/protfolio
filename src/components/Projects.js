af1879f0cb3e77bf6c69a5d18e55e4ae172a69c1
韓維 <hanwei@hans-mac.lan>
2022-10-17 00:27:50 -0500

    deploy

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
 import { CodeIcon } from "@heroicons/react/solid";
 import React from "react";
 import { projects } from "../data";
+import YoutubeEmbed from "./ytEmbed"
 
 export default function Projects() {
   return (
@@ -16,10 +17,13 @@ export default function Projects() {
           {projects.map((project) => (
             <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
               <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-                <img
+                {/* <img
                   className="w-full h-full"
                   src={project.image}
-                />
+                /> */}
+                <div class="aspect-w-16 aspect-h-9">
+                  <iframe src="https://www.youtube.com/embed/r9jwGansp1E" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
               </div>
               <div className="lg:w-full lg:w-full md:w-full w-full">
                 <h1 className="title-font text-3xl font-lg text-white mb-3">
